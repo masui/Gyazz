10e4e109c2d4ed42869ec104aacadfbd388dd878
Toshiyuki Masui <masui@pitecan.com>
2012-05-03 20:27:48 +0900

    /tmp/loglogに書くコードを消去

--- lib/writedata.rb
+++ lib/writedata.rb
@@ -38,9 +38,6 @@ def writedata(data)
   name = data.shift
   title = data.shift
   browser_md5 = data.shift
-  File.open("/tmp/loglog","a"){ |log|
-    log.puts "browser_md5 = #{browser_md5}"
-  }
   newdata = data.join("\n")+"\n"      # newdata: 新規書込みデータ
 
   newdata = data.join("\n").sub(/\n+$/,'')+"\n"      # newdata: 新規書込みデータ
@@ -59,10 +56,6 @@ def writedata(data)
     curdata = File.read(curfile).sub(/\n+$/,'')+"\n"
     server_md5 = md5(curdata)
   end                                 # curdata: Web上の最新データ
-  File.open("/tmp/loglog","a"){ |log|
-    log.puts "server_md5 = #{server_md5}"
-    log.puts "curdata = #{curdata}"
-  }
 
   # バックアップディレクトリを作成
   Dir.mkdir(backupdir(name)) unless File.exist?(backupdir(name))
@@ -75,22 +68,17 @@ def writedata(data)
     }
   end
 
-  status = '******'
-  File.open("/tmp/loglog","a"){ |log|
   if server_md5 == browser_md5 || curdata == '' then
-    log.puts "first if - noconflict"
     File.open(curfile,"w"){ |f|
       f.print(newdata)
     }
     status = 'noconflict'
   else
-    log.puts "second if"
     # ブラウザが指定したMD5のファイルを捜す
     oldfile = backupfiles(name,title).find { |f|
       md5(File.read(f)) == browser_md5
     }
     if oldfile then
-      log.puts "second if conflict"
       newfile = "/tmp/newfile#{$$}"
       patchfile = "/tmp/patchfile#{$$}"
       File.open(newfile,"w"){ |f|
@@ -101,7 +89,6 @@ def writedata(data)
       File.delete newfile, patchfile
       status = 'conflict'
     else
-      log.puts "second if noconflict"
       File.open(curfile,"w"){ |f|
         f.print newdata
       }
@@ -109,7 +96,6 @@ def writedata(data)
     end
     #status = 'conflict'
   end
-  }
 
   # 各行のタイムスタンプ保存
   timestamp = Time.now.strftime('%Y%m%d%H%M%S')
