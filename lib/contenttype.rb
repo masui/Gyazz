cda4049a4cd6e72b569cd3f3ad93ea98b96623fe
Toshiyuki Masui <masui@pitecan.com>
2012-10-23 07:19:08 +0900

    contenttype計算を奇麗にした

--- lib/contenttype.rb
+++ lib/contenttype.rb
@@ -1,42 +1,50 @@
-def contenttype(ext)
-  _contenttype={}
-  _contenttype['.txt'] = 'text/plain'
-  _contenttype['.csv'] = 'text/csv'
-  _contenttype['.tsv'] = 'text/tab-separated-values'
-  _contenttype['.doc'] = 'application/msword'
-  _contenttype['.xls'] = 'application/vnd.ms-excel'
-  _contenttype['.ppt'] = 'application/vnd.ms-powerpoint'
-  _contenttype['.pdf'] = 'application/pdf'
-  _contenttype['.xdw'] = 'application/vnd.fujixerox.docuworks'
-  _contenttype['.html'] = 'text/html'
-  _contenttype['.html'] = 'text/html'
-  _contenttype['.css'] = 'text/css'
-  _contenttype['.js'] = 'text/javascript'
-  _contenttype['.hdml'] = 'text/x-hdml'
-  _contenttype['.jpg'] = 'image/jpeg'
-  _contenttype['.jpeg'] = 'image/jpeg'
-  _contenttype['.png'] = 'image/png'
-  _contenttype['.gif'] = 'image/gif'
-  _contenttype['.bmp'] = 'image/bmp'
-  _contenttype['.ai'] = 'application/postscript'
-  _contenttype['.mp3'] = 'audio/mpeg'
-  _contenttype['.m4a'] = 'audio/mp4'
-  _contenttype['.wav'] = 'audio/x-wav'
-  _contenttype['.mid'] = 'audio/midi'
-  _contenttype['.midi'] = 'audio/midi'
-  _contenttype['.mmf'] = 'application/x-smaf'
-  _contenttype['.mpg'] = 'video/mpeg'
-  _contenttype['.mpeg'] = 'video/mpeg'
-  _contenttype['.wmv'] = 'video/x-ms-wmv'
-  _contenttype['.swf'] = 'application/x-shockwave-flash'
-  _contenttype['.3g2'] = 'video/3gpp2'
-  _contenttype['.zip'] = 'application/zip'
-  _contenttype['.lha'] = 'application/x-lzh'
-  _contenttype['.lzh'] = 'application/x-lzh'
-  _contenttype['.tar'] = 'application/x-tar'
-  _contenttype['.tgz'] = 'application/x-tar'
-  _contenttype['.exe'] = 'application/octet-stream'
-  _contenttype[''] = ''
-
-  return _contenttype[ext]
+# -*- coding: utf-8 -*-
+#
+# 拡張子からcontent-typeを得る
+#
+module ContentType
+  TYPE = {}
+  TYPE['.txt'] = 'text/plain'
+  TYPE['.csv'] = 'text/csv'
+  TYPE['.tsv'] = 'text/tab-separated-values'
+  TYPE['.doc'] = 'application/msword'
+  TYPE['.xls'] = 'application/vnd.ms-excel'
+  TYPE['.ppt'] = 'application/vnd.ms-powerpoint'
+  TYPE['.pdf'] = 'application/pdf'
+  TYPE['.xdw'] = 'application/vnd.fujixerox.docuworks'
+  TYPE['.html'] = 'text/html'
+  TYPE['.html'] = 'text/html'
+  TYPE['.css'] = 'text/css'
+  TYPE['.js'] = 'text/javascript'
+  TYPE['.hdml'] = 'text/x-hdml'
+  TYPE['.jpg'] = 'image/jpeg'
+  TYPE['.jpeg'] = 'image/jpeg'
+  TYPE['.png'] = 'image/png'
+  TYPE['.gif'] = 'image/gif'
+  TYPE['.bmp'] = 'image/bmp'
+  TYPE['.ai'] = 'application/postscript'
+  TYPE['.mp3'] = 'audio/mpeg'
+  TYPE['.m4a'] = 'audio/mp4'
+  TYPE['.wav'] = 'audio/x-wav'
+  TYPE['.mid'] = 'audio/midi'
+  TYPE['.midi'] = 'audio/midi'
+  TYPE['.mmf'] = 'application/x-smaf'
+  TYPE['.mpg'] = 'video/mpeg'
+  TYPE['.mpeg'] = 'video/mpeg'
+  TYPE['.wmv'] = 'video/x-ms-wmv'
+  TYPE['.swf'] = 'application/x-shockwave-flash'
+  TYPE['.3g2'] = 'video/3gpp2'
+  TYPE['.zip'] = 'application/zip'
+  TYPE['.lha'] = 'application/x-lzh'
+  TYPE['.lzh'] = 'application/x-lzh'
+  TYPE['.tar'] = 'application/x-tar'
+  TYPE['.tgz'] = 'application/x-tar'
+  TYPE['.exe'] = 'application/octet-stream'
+  TYPE[''] = ''
 end
+
+def contenttype(ext)
+  ContentType::TYPE[ext]
+end
+
+
