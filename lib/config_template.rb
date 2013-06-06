5bd0f9d65a047f5007a64760d23b0d5d58863b04
Sho Hashimoto <hashimoto@shokai.org>
2013-06-06 17:08:28 +0900

    use Rack::Session::Cookie instead of "enable :sessions"  #11

--- lib/config_template.rb
+++ lib/config_template.rb
@@ -5,3 +5,7 @@ FILEROOT = "/tmp"
 
 # DEFAULTPAGE = "/index.html"
 DEFAULTPAGE = "/Gyazz/目次"
+
+SESSION_DOMAIN = "gyazz.com"
+SESSION_KEY    = "__session_key_____(please_change)__"
+SESSION_SECRET = "__session_secret__(please_change)__"
