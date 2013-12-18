9b255ea97057a9a922250beae42fdac9bab02e19
Toshiyuki Masui <masui@pitecan.com>
2013-12-18 17:33:38 +0900

    modify.rb を access.rb に統合

--- lib/access.rb
+++ lib/access.rb
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
-require 'config'
 require 'lib'
 
 # アクセス履歴をJSONで返す
-
 def access(name,title)
   accessfile = "#{backupdir(name,title)}/access"
-  accessdata = []
-  accessdata = File.open(accessfile).read if File.exist?(accessfile)
-  "[\n" +
-    accessdata.collect { |line|
-      "\"#{line.chomp}\""
-    }.join(",\n") +
-    "\n]\n"
+  (File.exist?(accessfile) ? File.open(accessfile).read.split : []).to_json
 end
 
+# 変更履歴をJSONで返す
+def modify(name,title)
+  dir = backupdir(name,title)
+  return '' unless File.exist?(dir)
+  Dir.open(dir).find_all { |f|
+    f =~ /^\d{14}$/
+  }.sort { |a,b|
+    a <=> b
+  }.push(File.mtime(datafile(name,title)).strftime('%Y%m%d%H%M%S')).to_json
+end
