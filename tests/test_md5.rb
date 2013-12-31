4f8bc7b37d51bc101bd052942ceface62619961e
Toshiyuki Masui <masui@pitecan.com>
2013-12-31 20:46:30 +0900

    ...

--- tests/test_md5.rb
+++ tests/test_md5.rb
@@ -3,9 +3,11 @@ require File.expand_path 'test_helper', File.dirname(__FILE__)
 
 class MD5Test < MiniTest::Unit::TestCase
   def setup
+    @wiki = Gyazz::Wiki.new('test_wiki')
   end
 
   def teardown
+    system "/bin/rm -r -f #{@wiki.dir}"
   end
 
   def test_1
@@ -14,7 +16,6 @@ class MD5Test < MiniTest::Unit::TestCase
   end
 
   def test_2
-    wiki = Gyazz::Wiki.new('test_wiki')
-    assert Gyazz.id2title(wiki.name.md5) == wiki.name
+    assert Gyazz.id2title(@wiki.name.md5) == @wiki.name
   end
 end
