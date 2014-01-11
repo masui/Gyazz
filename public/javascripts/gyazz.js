b2c9b018fb0bcc59a2cc7e68caa89a4f14733d24
Toshiyuki Masui <masui@pitecan.com>
2014-01-11 10:31:43 +0900

    入力枠を操作するときは常にblur()するようにした

--- public/javascripts/gyazz.js
+++ public/javascripts/gyazz.js
@@ -211,8 +211,11 @@ $(document).keyup(function(event){
     //if(kc == 8) return;
     
     if(kc != 22 && kc != 21){
-        // 入力途中の文字列を確定 
-        data[editline] = $("input#newtext").val();
+        // 入力途中の文字列を確定
+	var input = $("input#newtext");
+	input.blur();
+        data[editline] = input.val();
+	input.focus();
     }
 
     if(kc == 13){ // 改行
